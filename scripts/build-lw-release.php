778d1abd5b4b868cccb28c143806f7bdeb064e2a
Alberto Ibarra <beto2l@me.com>
2026-09-05 18:48:14 -0500

    Return correct checkout error statuses

--- scripts/build-lw-release.php
+++ scripts/build-lw-release.php
@@ -50,7 +50,7 @@ $manifest = [
     'contract_version' => 2,
     'runtime' => 'php',
     'site' => 'pay-opin-x',
-    'version' => '1.0.2',
+    'version' => '1.0.3',
     'languages' => ['es'],
     'entries' => ['es' => 'step/recetario-keto/index.php'],
     'routes' => $routes,
--- scripts/process-payment.php
+++ scripts/process-payment.php
@@ -15,7 +15,11 @@ header('Content-Type: application/json; charset=utf-8');
  * Responde JSON y termina.
  */
 function json_out($data, $code = 200) {
-    http_response_code($code);
+    if (function_exists('status_header')) {
+        status_header($code);
+    } else {
+        http_response_code($code);
+    }
     echo json_encode($data, JSON_UNESCAPED_UNICODE);
     exit;
 }
